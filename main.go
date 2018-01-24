43dea962e200e168e49e1c16d10b094eb27b968e
LimaEchoCharlie <LimaEchoCharlie@outlook.com>
2018-01-24 23:39:40 +0000

    Add config and restart server on failure

--- main.go
+++ main.go
@@ -1,17 +1,47 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
-func main() {
-	http.HandleFunc("/howdy", howdy)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+type Configuration struct {
+	Latitude  float64 // the latitude of the device
+	Longitude float64 // the longitude of the device
+}
+
+var config = Configuration{}
+
+// loadConfiguration loads the server configuration
+func loadConfiguration() error {
+	// open configuration file
+	// TODO: remove the need to hardcode location for systemd
+	file, err := os.Open("/home/pi/configuration.json")
+	if err != nil {
+		return err
+	}
+
+	// decode json
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&config)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 // howdy echoes howdy
 func howdy(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Howdy, this is Heihei\n")
+	fmt.Fprintf(w, "Howdy, this is Heihei at %v and %v\n", config.Latitude, config.Longitude)
+}
+
+func main() {
+	if err := loadConfiguration(); err != nil {
+		log.Fatal(err)
+	}
+	http.HandleFunc("/howdy", howdy)
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
